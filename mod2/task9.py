a5b7f5156a8e1f58f4dccf26207908d9610d7966
ihelops <ihelops@yandex.ru>
2025-03-18 17:01:21 +0500

    Избавился от библиотеки в 9 задании mod2

--- mod2/task9.py
+++ mod2/task9.py
@@ -1,11 +1,6 @@
-import re
-
 def clean_phone_number(phone):
-    cleaned = re.sub(r'[^0-9+]', '', phone)
+    cleaned = ''.join(c for c in phone if c.isdigit() or c == '+')
     print(cleaned)
 
-def main():
-    phone = input().strip()
-    clean_phone_number(phone)
-
-main()
+phone = input().strip()
+clean_phone_number(phone)
